857b2143c34ce9cb38fe880e514d4070a0d63d15
SanF <464884785@qq.com>
2019-10-02 02:19:53 +0800

    d

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-// webpack.config.js
-module.exports = {
-       output:{
-        library: 'join-ui',
-        // 使用非闭包方式 解决 import {} from '' undefined
-        libraryTarget: 'umd',
-        filename: 'join-ui.js',
-    }
-  }
