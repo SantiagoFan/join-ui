e5aaa90217669a20ad95b5a83f227c2d1948fe8e
SanF <464884785@qq.com>
2019-10-09 15:37:51 +0800

    增加依赖

--- examples/App.vue
+++ examples/App.vue
@@ -5,27 +5,20 @@
       <HelloWorld msg="JOIN-UI 组件示例"/>
     </div>
     <h2>1.juiQrcode</h2>
-    <jui-qrcode text="http://www.nmgjoin.com"></jui-qrcode>
-     <h2>2.SkuSpec</h2>
-    <sku-spec v-model="sku_keys"></sku-spec>
-    <h2>3.SkuTable</h2>
-    <sku-table v-model="sku_values" :specification.sync="sku_keys"></sku-table>
-   
+    <qrcode text="http://www.nmgjoin.com"></qrcode>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 // 按需引入
-import  { juiQrcode,SkuTable,SkuSpec }    from '~'
+import  { Qrcode }    from '~'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
-    juiQrcode,
-    SkuTable,
-    SkuSpec
+    Qrcode,
   },
   data(){
     return{
--- packages/index.js
+++ packages/index.js
@@ -1,37 +1,33 @@
-// 导入日期选择器组件
-import juiQrcode from './qrcode/index.js'
-import { SkuTable,SkuSpec } from './sku/index.js'
-
+/* eslint-disable */
+// This file is auto gererated by build/build-entry.js
+import Qrcode from './qrcode'
+const version = '0.1.8'
 // 存储组件列表
 const components = [
-  juiQrcode,
-  SkuTable,
-  SkuSpec,
-  ];
-
+  Qrcode
+]
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
-const install = function (Vue) {
+const install = Vue => {
   // 判断是否安装
   if (install.installed) return
   // 遍历注册全局组件
-  components.map(component => Vue.component(component.name, component))
-}
+  components.forEach(Component => {
+    Vue.use(Component)
+  })
+};
 
+/* istanbul ignore if */
 // 判断是否是直接script引入文件
 if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue)
 }
-
-
-export default {
+export {
   // 导出的对象必须具有 install，才能被 Vue.use() 方法安装
   install,
-  // 以下是具体的组件列表
-  ...components
+  version,
+  Qrcode
 }
-
-export {
-  juiQrcode,
-  SkuTable,
-  SkuSpec,
+export default {
+  install,
+  version
 }
