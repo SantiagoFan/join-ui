878c79e8cc7de44da002f6067583767e75ad2b8f
SanF <464884785@qq.com>
2019-10-16 12:00:43 +0800

    push 0.19

--- packages/index.js
+++ packages/index.js
@@ -8,20 +8,23 @@ const components = [
   Btn,
   Qrcode
 ]
+// 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
 const install = Vue => {
+  // 判断是否安装
+  if (install.installed) return
   // 遍历注册全局组件
   components.forEach(Component => {
-    console.info('注册'+Component.name)
-
     Vue.use(Component)
   })
 };
 
 /* istanbul ignore if */
+// 判断是否是直接script引入文件
 if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue)
 }
 export {
+  // 导出的对象必须具有 install，才能被 Vue.use() 方法安装
   install,
   version,
   Btn,
