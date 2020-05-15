7b54f0250d2a9f06967b7783567eee916761a265
SanF <464884785@qq.com>
2020-05-15 16:22:00 +0800

    x

--- packages/SkuSpec/index.js
+++
@@ -1,9 +0,0 @@
-// 导入组件，组件必须声明 name
-import SkuSpec from './SkuSpec.vue'
-
-// 为组件提供 install 安装方法，供按需引入
-SkuSpec.install = function (Vue) {
-  Vue.component(SkuSpec.name, SkuSpec)
-}
-
-export default SkuSpec
--- packages/SkuTable/index.js
+++
@@ -1,9 +0,0 @@
-// 导入组件，组件必须声明 name
-import SkuTable from './SkuTable.vue'
-
-// 为组件提供 install 安装方法，供按需引入
-SkuTable.install = function (Vue) {
-  Vue.component(SkuTable.name, SkuTable)
-}
-
-export default SkuTable
